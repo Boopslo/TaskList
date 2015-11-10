ac79b191cef730977e4245f166fca4455f555d3a
Boopslo <oslophantom@gmail.com>
2015-11-10 14:15:58 -0500

    Initial Commit

---
+++ ShihChi_Lin_Lab4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShihChi_Lin_Lab4
+//
+//  Created by Oslo on 11/10/15.
+//  Copyright © 2015 Shih Chi Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShihChi_Lin_Lab4Tests/ShihChi_Lin_Lab4Tests.m
@@ -0,0 +1,39 @@
+//
+//  ShihChi_Lin_Lab4Tests.m
+//  ShihChi_Lin_Lab4Tests
+//
+//  Created by Oslo on 11/10/15.
+//  Copyright © 2015 Shih Chi Lin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ShihChi_Lin_Lab4Tests : XCTestCase
+
+@end
+
+@implementation ShihChi_Lin_Lab4Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
